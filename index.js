9eb77b22e196d781d9785a0e4ab91a0703f85997
khjkr <49487372+khjkr@users.noreply.github.com>
2019-09-28 16:27:10 +0900

    Update code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const TextChannel = Discord.TextChannel
 const client = new Discord.Client()
 const config = require('./lib/config.json')
 
@@ -6,10 +7,31 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', msg => {
-  if(msg.content.startsWith(config.prefix + '공지' || config.prefix + 'notice')) {
-    msg.reply('Test!')
-  }
+client.on('message', message => {
+    if(message.author.id !== config.owner) return;
+    if(message.content.startsWith(config.prefix + '공지' || config.prefix + 'notice')) {
+        message.channel.send('The notice is being sent...').then(e =>{
+            const embed = new Discord.RichEmbed()
+              .setTitle('📢**'+config.title+'**')
+              .setColor(config.color)
+              .setFooter(`${message.author.tag} - Verified.`)
+              .setDescription(message.content.slice(message.content.replace('\n', ' ').split(' ')[0].length, message.content.length))
+        
+        client.guilds.forEach(guild => {
+            for(let i in config.channel){
+                const gchannel = guild.channels.find(
+                    val => (
+                        val.name.includes(config.channel[i])) && val instanceof TextChannel
+                    )
+
+        if(gchannel instanceof TextChannel) {
+            gchannel.send(embed)
+        } else return
+    }
+})
+            e.edit(':white_check_mark: Notice has been sent!')
+        })
+    }
 })
 
 client.login(config.token)
